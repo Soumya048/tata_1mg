f96128c3691abc7a4e062c41aef4b4506c63987d
vaibhav joshi <vaibhavj478@gmail.com>
2022-06-18 21:55:55 +0530

    add footer

---
+++ components/footer.js
@@ -0,0 +1,406 @@
+
+
+const footerFun =()=>{
+
+    return `
+    
+
+  
+    <section class="style__contentSection___2fS9b">
+      <h2>
+        Stay Healthy with Tata 1mg: Your Favourite Online Pharmacy and
+        Healthcare Platform
+      </h2>
+      <h3>We Bring Care to Health.</h3>
+      <p>
+        Tata 1mg is India's leading digital healthcare platform. From doctor
+        consultations on chat to online pharmacy and lab tests at home: we
+        have it all covered for you. Having delivered over 25 million orders
+        in 1000+ cities till date, we are on a mission to bring "care" to
+        "health" to give you a flawless healthcare experience.
+      </p>
+      <h3>Tata 1mg: Your Favourite Online Pharmacy!</h3>
+      <p>
+        Tata 1mg is India's leading online chemist with over 2 lakh medicines
+        available at the best prices. We are your one-stop destination for
+        other healthcare products as well, such as over the counter
+        pharmaceuticals, healthcare devices and homeopathy and ayurveda
+        medicines.
+      </p>
+      <p>
+        With Tata 1mg, you can buy medicines online and get them delivered at
+        your doorstep anywhere in India! But, is ordering medicines online a
+        difficult process? Not at all! Simply search for the products you want
+        to buy, add to cart and checkout. Now all you need to do is sit back
+        as we get your order delivered to you.
+      </p>
+      <p>
+        In case you need assistance, just give us a call and we will help you
+        complete your order.
+      </p>
+      <p>
+        Don't want to go through the hassle of adding each medicine
+        separately? You can simply upload your prescription and we will place
+        your order for you. And there is more! At Tata 1mg, you can buy health
+        products and medicines online at best discounts.
+      </p>
+      <p>
+        Now, isn't that easy? Why go all the way to the medicine store and
+        wait in line, when you have Tata 1mg Pharmacy at your service.
+      </p>
+      <h3>The Feathers in Our Cap</h3>
+      <p>
+        At Tata 1mg, our goal is to make healthcare understandable, accessible
+        and affordable in India. We set out on our journey in 2015, and have
+        come a long way since then. Along the way, we have been conferred with
+        prestigious titles like
+        <b
+          >BML Munjal Award for 'Business Excellence through Learning and
+          Development', Best Online Pharmacy in India Award</b
+        >
+        <!-- -->and <b>Top 50 venture in The Smart CEO-Startup50 India.</b>
+        <!-- -->We have been selected as the only company from across the
+        globe for<b>
+          <!-- -->SD#3 "Health &amp; Well Being for all" by Unreasonable
+          group, US State Department.</b
+        >
+        <!-- -->In 2019 alone we received three awards including the<b>
+          <!-- -->BMW Simply Unstoppable Award.</b
+        >
+      </p>
+      <h3>The Services We Offer</h3>
+      <p>
+        Tata 1mg is India's leading digital healthcare platform, where you can
+        buy medicines online with discount. Buy medicine online in Delhi,
+        Mumbai, Bangalore, Hyderabad, Pune, Gurgaon, Noida, Kolkata, Chennai,
+        Ahmedabad, Lucknow and around a 1000 more cities. Besides delivering
+        your online medicine order at your doorstep, we provide accurate,
+        authoritative &amp; trustworthy information on medicines and help
+        people use their medicines effectively and safely.
+      </p>
+      <p>
+        We also facilitate lab tests at home. You can avail over 2,000 tests
+        and get tested by 120+ top and verified labs at the best prices. Need
+        to consult a doctor? On our platform, you can talk to over 20 kinds of
+        specialists in just a few clicks.
+      </p>
+      <p>
+        Customer centricity is the core of our values. Our team of highly
+        trained and experienced doctors, phlebotomists and pharmacists looks
+        into each order to give you a fulfilling experience.
+      </p>
+      <p>
+        We’ve made healthcare accessible to millions by giving them quality
+        care at affordable prices. Customer centricity is the core of our
+        values. Our team of highly trained and experienced doctors,
+        phlebotomists and pharmacists looks into each order to give you a
+        fulfilling experience.
+      </p>
+      <p>
+        Visit our online medical store now, and avail online medicine purchase
+        at a discount.<br />Stay Healthy!
+      </p>
+    </section>
+
+    <footer
+      id="footer-bar"
+      class="style__footer-container___1uRLj style__black-color___MucSg style__f14___1FE-V"
+    >
+      <div class="style__top-bar___5ZPPN">
+        <div class="style__top-head___2h8g-">
+          <h2>INDIA’S LARGEST HEALTHCARE PLATFORM</h2>
+        </div>
+        <div class="style__flex-box____V1Y1 style__stats-bar___w397H">
+          <div>
+            <div class="style__stats-num___3Ns_4">260m+</div>
+            <div class="style__stats-text___DUvDf">Visitors</div>
+          </div>
+          <div>
+            <div class="style__stats-num___3Ns_4">31m+</div>
+            <div class="style__stats-text___DUvDf">Orders Delivered</div>
+          </div>
+          <div>
+            <div class="style__stats-num___3Ns_4">1800+</div>
+            <div class="style__stats-text___DUvDf">Cities</div>
+          </div>
+        </div>
+        <div class="style__border-btm___1wdot"></div>
+        <div class="style__flex-box____V1Y1 style__down-bar___3FV6g">
+          <div class="style__free-text___3jZ7n style__f20___2opYM">
+            Get the link to download App
+          </div>
+          <div class="style__input-box___7akGm">
+            <form>
+              <input type="tel" id="phoneNumber" class="style__input-phone___2P9PU" placeholder="Enter Phone Number"
+                maxlength="10" autocomplete="off"  value=""  pattern="[6-9]\d{9}"
+                title="Please enter valid 10 digits" required=""
+              /><button type="submit" class="style__next-button___3J4Nx">
+                Send Link
+              </button>
+            </form>
+          </div>
+          <div class="style__cap-wrap___38IWm"></div>
+        </div>
+      </div>
+      <div class="style__flex-box____V1Y1 style__main-link-bar___12igi">
+        <div class="style__main-content___2sd49">
+          <div>
+            <h3 class="style__f16___7B4eA style__fweight500___oy16u">
+              Know Us
+            </h3>
+            <ul>
+              <li>
+                <a href="/aboutUs"><span>About Us</span></a>
+              </li>
+              <li>
+                <a href="/contactUs"><span>Contact Us</span></a>
+              </li>
+              <li>
+                <a href="/press-coverage"><span>Press Coverage</span></a>
+              </li>
+              <li>
+                <a href="/jobs"><span>Careers</span></a>
+              </li>
+              <li>
+                <a href="https://partners.1mg.com"
+                  ><span>Business Partnership</span></a
+                >
+              </li>
+              <li>
+                <a href="/healthpartner"
+                  ><span>Become a Health Partner</span></a
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="style__main-content___2sd49">
+          <div>
+            <h3 class="style__f16___7B4eA style__fweight500___oy16u">
+              Our Policies
+            </h3>
+            <ul>
+              <li>
+                <a href="/PrivacyPolicy"><span>Privacy Policy</span></a>
+              </li>
+              <li>
+                <a href="/Tnc"><span>Terms and Conditions </span></a>
+              </li>
+              <li>
+                <a href="/editorial-policy-processes"
+                  ><span>Editorial Policy</span></a
+                >
+              </li>
+              <li>
+                <a href="/return-policy"><span>Return Policy</span></a>
+              </li>
+              <li>
+                <a href="/ip-policy"><span>IP Policy</span></a>
+              </li>
+              <li>
+                <a href="/grievance-redressal-policy"
+                  ><span>Grievance Redressal Policy</span></a
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="style__main-content___2sd49">
+          <div>
+            <h3 class="style__f16___7B4eA style__fweight500___oy16u">
+              Our Services
+            </h3>
+            <ul>
+              <li>
+                <a href="/"><span>Order Medicines</span></a>
+              </li>
+              <li>
+                <a href="/labs"><span>Book Lab Tests</span></a>
+              </li>
+              <li>
+                <a href="/online-doctor-consultation"
+                  ><span>Consult a Doctor</span></a
+                >
+              </li>
+              <li>
+                <a href="/ayurveda"><span>Ayurveda Articles</span></a>
+              </li>
+              <li>
+                <a href="/hi/patanjali"><span>Hindi Articles</span></a>
+              </li>
+              <li>
+                <a href="/subscription-plan/subscriptions"
+                  ><span>Care Plan</span></a
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="style__main-content___2sd49">
+          <div>
+            <h3 class="style__f16___7B4eA style__fweight500___oy16u">
+              Connect
+            </h3>
+            <ul>
+              <li><span class="style__s-head___367TK">Social Links</span></li>
+              <li class="style__m-web___2xu0C">
+                <a
+                  href="https://www.facebook.com/1mgOfficial"
+                  alt="Facebook"
+                  rel="nofollow"
+                  target="_blank"
+                  ><div class="facebook"></div></a
+                ><a
+                  href="https://twitter.com/1mgofficial"
+                  alt="Twitter"
+                  rel="nofollow"
+                  target="_blank"
+                  ><div class="twitter"></div></a
+                ><a
+                  href="https://www.youtube.com/c/1mgofficial"
+                  alt="You Tube"
+                  rel="nofollow"
+                  target="_blank"
+                  ><div class="youtube"></div></a
+                ><a
+                  href="https://www.linkedin.com/company/1mg"
+                  alt="Linkedin"
+                  rel="nofollow"
+                  target="_blank"
+                  ><div class="linkedin"></div></a
+                ><a
+                  href="https://medium.com/1mgofficial"
+                  alt="Medium Link"
+                  rel="nofollow"
+                  target="_blank"
+                  ><div class="medium"></div
+                ></a>
+              </li>
+              <li>
+                <span class="style__grey-txt___2FAow"
+                  >Want daily dose of health?</span
+                >
+              </li>
+              <li>
+                <button class="style__signup-btn___2h_dH">SIGN UP</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="style__main-content___2sd49">
+          <div>
+            <h5>Download App</h5>
+            <ul>
+              <li>
+                <a
+                  href="https://play.google.com/store/apps/details?id=com.aranoah.healthkart.plus"
+                  ><div class="google_play style__m12___25Let"></div
+                ></a>
+              </li>
+              <li>
+                <a href="https://itunes.apple.com/in/app/1mg/id554578419?mt=8"
+                  ><div class="apple_store"></div
+                ></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="style__auth-bar___bYPU8">
+        <div class="style__auth-cont___2vf7G">
+          <div class="reliable"></div>
+          <div class="style__details-wrapper___3X0Wt">
+            <h5 class="style__f18___1g_my">Reliable</h5>
+            <p class="style__f12___3AbTH style__grey-txt___2FAow">
+              All products displayed on Tata 1mg are procured from verified
+              and licensed pharmacies. All labs listed on the platform are
+              accredited
+            </p>
+          </div>
+        </div>
+        <div class="style__auth-cont___2vf7G">
+          <div class="secure"></div>
+          <div class="style__details-wrapper___3X0Wt">
+            <h5 class="style__f18___1g_my">Secure</h5>
+            <p class="style__f12___3AbTH style__grey-txt___2FAow">
+              Tata 1mg uses Secure Sockets Layer (SSL) 128-bit encryption and
+              is Payment Card Industry Data Security Standard (PCI DSS)
+              compliant
+            </p>
+          </div>
+        </div>
+        <div class="style__auth-cont___2vf7G">
+          <div class="affordable"></div>
+          <div class="style__details-wrapper___3X0Wt">
+            <h5 class="style__f18___1g_my">Affordable</h5>
+            <p class="style__f12___3AbTH style__grey-txt___2FAow">
+              Find affordable medicine substitutes, save up to 50% on health
+              products, up to 80% off on lab tests and free doctor
+              consultations.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="style__trust-bar___1TfPG">
+        <div class="style__trust-stamps____4BQl">
+          <div class="style__legitscript___2ZisC style__with-legitscript___2Tut6">
+            <div>
+              <!-- <script src="https://static.legitscript.com/seals/729605.js"></script> -->
+              <a href="https://legitscript.com" target="_blank" title="Verify LegitScript Approval" >
+                <img src="https://static.legitscript.com/seals/729605.png" alt="LegitScript approved" width="140" height="100" border="0" />
+              </a>
+            </div>
+          </div>
+
+
+          <div class="style__with-legitscript___2Tut6">
+            <div class="style__image-container___Nt9vX">
+              <div class="">
+                <img alt='We are certified "ISO/IEC 27001:2013"' src="https://res.cloudinary.com/du8msdgbj/image/upload/v1580717209/URS_ebgmxc.png" title='We are certified "ISO/IEC 27001:2013"' class="style__scale-down-9___2X_aX style__image___Ny-Sa style__loaded___22epL" />
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+        <div class="style__free-text3___3upNE">
+          India's only LegitScript and ISO/ IEC 27001 certified online
+          healthcare platform
+        </div>
+      </div>
+      <div class="style__collapse___1sCdv">
+        <div class="style__f-head___2SFeL">
+          <h2 class="style__know-text___1bUL7">
+            <!-- -->Know more about Tata 1mg<span
+              class="style__b-collapse___3gaL1"
+              ><span
+                ><img
+                  src="https://img.1mg.com/images/down-arrowRebrand.svg"
+                  alt="downArrow" /></span
+            ></span>
+          </h2>
+        </div>
+
+
+        
+
+
+      <div class="style__flex-box____V1Y1 style__b-text___uV-wn">
+        <div>
+          <div class="style__copyright___qclAW">
+            © 2022 Tata 1mg. All rights reserved. In compliance with Drugs and
+            Cosmetics Act, 1940 and Drugs and Cosmetics Rules, 1945, we don't
+            process requests for Schedule X and other habit forming drugs.
+          </div>
+        </div>
+        <div class="payment"></div>
+      </div>
+    </footer>
+ 
+
+    `
+}
+
+
+
+export default footerFun;
--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,8 @@ import { pageTitle, pageFavion } from "../components/title.js";
 
 import dropDownNavbarEle from "../components/dropDownNavbar.js";
 
+
+
 // landing page work
 import {
   featuredBrandsOne,
@@ -12,6 +14,13 @@ import {
 
 
 } from "../components/landingPageData.js";
+
+
+// import footer 
+
+import footerFun  from "../components/footer.js"
+
+
 
 pageTitle(
   "Online Pharmacy India | Buy Medicines from India's Trusted Medicine Store: 1mg.com"
@@ -30,18 +39,7 @@ dropDownNavbarEle().forEach((ele, ind) => {
   dropDownNavbar.append(ele);
 });
 
-//  let featuredBrands =[
-//     {
-//         img: "https://onemg.gumlet.io/image/upload/w_150,h_150,c…_auto,f_auto/v1655447476/kkpsr5tdl5bsc16ymrlt.jpg",
-//         name:`Baidyanath(Nagpur)`,
-//     },
 
-//     {
-//         img: "https://onemg.gumlet.io/image/upload/w_150,h_150,c…_auto,f_auto/v1655447476/kkpsr5tdl5bsc16ymrlt.jpg",
-//         name:`Baidyanath(Nagpur)`,
-//     },
-
-//  ]
 
 
 const displayOne = (str,arr) => {
@@ -119,19 +117,22 @@ const displayTwo = (str,arr) => {
   });
 };
 
-// console.log(featuredBrandsOne,featuredBrandsTwo);
-
-let oneOne = document.querySelector("#oneOne");
-displayOne(oneOne,featuredBrandsOne);
-
-let oneTwo = document.querySelector("#oneTwo");
-displayTwo( oneTwo,featuredBrandsTwo);
 
 
-let twoOne = document.querySelector("#twoOne");
-displayOne(twoOne,popularCategoriesOne);
 
-let twoTwo = document.querySelector("#twoTwo");
-displayTwo( twoTwo,popularCategoriesTwo);
+// slide call
 
 
+
+
+// appending footer 
+
+let footer = document.querySelector("#tata1mgFooter")
+
+// console.log(footer);
+
+footer.innerHTML = footerFun();
+
+// console.log(footerFun());
+
+//  footerFun();
