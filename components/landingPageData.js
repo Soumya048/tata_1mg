137a36e666ffa10ddd82b1b7f2920bc03dd5642c
vaibhav joshi <vaibhavj478@gmail.com>
2022-06-19 15:25:25 +0530

    data

--- components/landingPageData.js
+++ components/landingPageData.js
@@ -183,27 +183,27 @@ shopByHealthConcernsOne = [
 
     {
         img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624524227/hgzkyio2rphkn6wjif94.png`,
-        name:``
+        name:`Cardiac Care`
     },
     {
         img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624524269/zo7sc14nqpt6ebeojkvr.png`,
-        name:``
+        name:`Stomach Care`
     },
     {
         img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624524293/ttagxlnxulsvkyv9fetr.png`,
-        name:``
+        name:`Liver Care`
     },
     {
         img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624524308/ijgn7fhyq8ig0welmrcp.png`,
-        name:``
+        name:`Kidney Care`
     },
     {
         img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624524282/zytsldnf5mvb0u19ydep.png`,
-        name:``
+        name:`Bone, Joint & Muscle Care`
     },
     {
         img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624524210/ywohqm4txnehsmibypaz.png`,
-        name:``
+        name:`Pain Relief`
     },
     {
         img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624524252/hn5bpr1ci2do5kdub51v.png`,
@@ -214,32 +214,390 @@ shopByHealthConcernsOne = [
 shopByHealthConcernsTwo = [
 
     {
-        img:``,
-        name:``
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624524293/ttagxlnxulsvkyv9fetr.png`,
+        name:`Stomach Care`
     },
     {
-        img:``,
-        name:``
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624524308/ijgn7fhyq8ig0welmrcp.png`,
+        name:`Liver Care`
     },
     {
-        img:``,
-        name:``
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624524282/zytsldnf5mvb0u19ydep.png`,
+        name:`Kideny Care`
     },
     {
-        img:``,
-        name:``
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624524210/ywohqm4txnehsmibypaz.png`,
+        name:`Bone,Joint & Muscle Care`
     },
     {
-        img:``,
-        name:``
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624524252/hn5bpr1ci2do5kdub51v.png`,
+        name:`Pain Relief`
     },
     {
-        img:``,
-        name:``
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624524188/hr9vysj9cy0sfjs3mpg0.png`,
+        name:`Respiratory Care`
     },
     {
-        img:``,
-        name:``
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624524365/t0ehbhfrkughfxoygvs2.png`,
+        name:`Eye Care`
     },    
 
+]
+
+
+let mineralSupplementsOne = [
+    {
+        img: `https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624974568/bknmwxqz9am8bdkprywr.png`,
+        name:`Multivitamins`,
+    },
+
+    {
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624974052/jdrht4hevhajzejbyf6u.png`,
+        name:`Iron`,
+    },
+
+    {
+        img:` https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624974226/oqimcsqluesicdqn2tdl.png`,
+        name:`Zinc`,
+    },
+
+    {
+        img:` https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624974548/s1hxii1s0js4h89rxh5g.png`,
+        name:`Calcium`,
+    },
+    {
+        img:` https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624974250/kpyknruehq4ncpelamxv.png`,
+        name:`Magnesium`,
+    },
+    {
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624974301/g41rbljmnuasdmtp20yc.png `,
+        name:`Potassium`,
+    },
+    {
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624974370/roleuut74izomc68as5l.png `,
+        name:`Selenium`,
+    },
+]
+
+
+let mineralSupplementsTwo = [
+   
+
+    {
+        img:` https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624974226/oqimcsqluesicdqn2tdl.png`,
+        name:`Zinc`,
+    },
+
+    {
+        img:` https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624974548/s1hxii1s0js4h89rxh5g.png`,
+        name:`Calcium`,
+    },
+    {
+        img:` https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624974250/kpyknruehq4ncpelamxv.png`,
+        name:`Magnesium`,
+    },
+    {
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624974301/g41rbljmnuasdmtp20yc.png `,
+        name:`Potassium`,
+    },
+    {
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624974370/roleuut74izomc68as5l.png `,
+        name:`Selenium`,
+    },
+
+
+    {
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624974407/sfmufp9hh86ii2k453tg.png `,
+        name:`Iodine`,
+    },
+
+    {
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1624974528/e4xwvfzuciykpp4qoudo.png `,
+        name:`Copper`,
+    },
+
+]
+
+
+
+let summerEssentialsOne =[
+    {
+
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1652864862/smypjo9cnztpel40vu3t.png`,
+        name:`Sunscreen Products`,
+
+    },
+
+    {
+
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1652864884/exf055so5zdf8uxdr6d7.png`,
+        name:`Glucose & Energy Drink`,
+
+    },
+
+    {
+
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1652864911/meyblvct5mizwray9i7r.png`,
+        name:`Talcum Powder`,
+
+    },
+
+    {
+
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1652864944/rradglaxviwl6ekcr3ha.png`,
+        name:`Body wash, Soaps`,
+
+    },
+
+    {
+
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1655284177/ccw5fcbqzn4xix9zafln.png`,
+        name:`Antifungal Creams`,
+
+    },
+
+    {
+
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1652865020/guevfm39u0cmiwds89ha.png`,
+        name:`Facewash`,
+
+    },
+
+    {
+
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1652865033/mjcodampi5ezyrhpuaxs.png`,
+        name:`Herbal Juices`,
+
+    },
+
+
+    
+
+]
+
+
+let summerEssentialsTwo =[
+    
+
+    {
+
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1652864911/meyblvct5mizwray9i7r.png`,
+        name:`Talcum Powder`,
+
+    },
+
+    {
+
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1652864944/rradglaxviwl6ekcr3ha.png`,
+        name:`Body wash, Soaps`,
+
+    },
+
+    {
+
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1655284177/ccw5fcbqzn4xix9zafln.png`,
+        name:`Antifungal Creams`,
+
+    },
+
+    {
+
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1652865020/guevfm39u0cmiwds89ha.png`,
+        name:`Facewash`,
+
+    },
+
+    {
+
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1652865033/mjcodampi5ezyrhpuaxs.png`,
+        name:`Herbal Juices`,
+
+    },
+    {
+
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1652864972/aagvjadbb1vhf2soi3dm.png`,
+        name:`Mosquito Repellents`,
+
+    },
+    {
+
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1652865100/gp8zovwmiv3cttmuu12s.png`,
+        name:` Ayurveda Personal Care`,
+
+    },
+    
+
+]
+
+
+let exploreMoreInPersonalCareOne = [
+    {
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1652877303/sxt3g96s20d2nfskgp0x.png`,
+        name:`Hair Care`,
+    },
+
+    {
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1652877325/e3ykjhrf8bkyya4zyb6u.png`,
+        name:`Baby Care`,
+    },
+
+    {
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1652877340/uqxrou7qwqjvfkx8gutu.png`,
+        name:`Women Care`,
+    },
+
+    {
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1652877358/otvmyyoi3s6eydvpuzom.png`,
+        name:`Men Care`,
+    },
+
+    {
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1652877379/aprpu3dnttzmletf7lp4.png`,
+        name:`Oral Care`,
+    },
+
+    {
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1654078448/hqveroe6dy5aeuel1rt0.png`,
+        name:`Homeopathy Hair & Skin`,
+    },
+
+    {
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1653306835/qqbgcvyldtznri7dy88r.png`,
+        name:`Summer Essentail `,
+    },
+
+]
+
+let exploreMoreInPersonalCareTwo = [
+   
+
+    {
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1652877340/uqxrou7qwqjvfkx8gutu.png`,
+        name:`Women Care`,
+    },
+
+    {
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1652877358/otvmyyoi3s6eydvpuzom.png`,
+        name:`Men Care`,
+    },
+
+    {
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1652877379/aprpu3dnttzmletf7lp4.png`,
+        name:`Oral Care`,
+    },
+
+    {
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1654078448/hqveroe6dy5aeuel1rt0.png`,
+        name:`Homeopathy Hair & Skin`,
+    },
+
+    {
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1653306835/qqbgcvyldtznri7dy88r.png`,
+        name:`Summer Essentail `,
+    },
+
+
+    {
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1653306852/brgt0vgcjfdr0nrzyuxo.png`,
+        name:`Derma Care`,
+    },
+
+    {
+        img:`https://onemg.gumlet.io/image/upload/w_150,h_150,c_fit,a_ignore,q_auto,f_auto/v1653980240/rtpncqjoh9nfqlxlbi6w.png`,
+        name:`Ayurveda Personal Care`,
+    },
+
+]
+
+
+
+
+let AyurvedaOne =[
+    {
+        img:`https://res.cloudinary.com/du8msdgbj/image/upload/v1640843640/xmsbbrvwsnkcql9d58rj.jpg`,
+        name:`Gambhari`,
+        para:`Gambhari is commonly known as the Chandahara tree. The tree grows in the hilly areas ...`,
+    },
+    {
+        img:`https://onemg.gumlet.io/image/upload/qscymurwud6nrbttg0wo.jpg`,
+        name:`Arogyavardhini Vati`,
+        para:`Arogyavardhini Vati as suggested by its name “Arogya-good health, Vardhini-improves” ...`,
+    },
+    {
+        img:`https://onemg.gumlet.io/image/upload/zfrnjskvt0xdka0cuq52.jpg`,
+        name:`Chir`,
+        para:`Chir or Chir pine tree is an economically valuable species commonly found in the Hima...`,
+    },
+    {
+        img:`https://onemg.gumlet.io/image/upload/ofggscumvx7ofmbp29sx.jpg`,
+        name:`Pomegranate`,
+        para:`Pomegranate is a treasure of nutrients, known as “Dadima” in Ayurveda, that has been ...`,
+    },
+
+]
+
+let AyurvedaTwo =[
+   
+    {
+        img:`https://onemg.gumlet.io/image/upload/gxhv6gcq41rurkyluzda.jpg`,
+        name:`Kuth`,
+        para:`Kuth or Kustha is a potent plant which is known for its medicinal uses. 
+        Kuth helps t...`,
+    },
+    {
+        img:`https://onemg.gumlet.io/image/upload/g78jwrcji89a6xsn7kzz.jpg`,
+        name:`Milk thistle`,
+        para:`Milk thistle, commonly known as silymarin, is a plant named for the white veins on it...`,
+    },
+    {
+        img:`https://onemg.gumlet.io/image/upload/zbnyg5jpmporvzpazpsf.jpg`,
+        name:`Green Coffee`,
+        para:`Green coffee is a popular health supplement. It is the unroasted form of coffee beans...`,
+    },
+    {
+        img:`https://onemg.gumlet.io/image/upload/asmr8ybzlut9ghc5btx2.jpg`,
+        name:`Kokum`,
+        para:`Kokum is also known as the “Indian Butter Tree” and is a fruit-bearing tree. All part...`,
+    },
+
+]
+
+
+
+let radiologyTestsUpTo60One =[
+    {
+        title:`Usg Whole Abdomen`,
+        para:`Ultrasound Whole Abdomen/ Sonography Whole Abdomen`,
+        providedby:`Provided By 14 Labs`,
+        mrf:`840`,
+        off:`40% Off`,
+        img:`https://res.cloudinary.com/du8msdgbj/image/upload/v1590653294/safe_bjisiz.svg`,
+    },
+
+
+    {
+        title:`Electrocardiography`,
+        para:`Electrocardiogram`,
+        providedby:`Provided By 9 Labs`,
+        mrf:`840`,
+        off:`40% Off`,
+        img:`https://res.cloudinary.com/du8msdgbj/image/upload/v1590653294/safe_bjisiz.svg`,
+    },
+    {
+        title:`MRI Brain`,
+        para:`aidkm kMWDJW FEFJDAN JIDKNADAND`,
+        providedby:`Provided By 7 Labs`,
+        mrf:`840`,
+        off:`40% Off`,
+        img:`https://res.cloudinary.com/du8msdgbj/image/upload/v1590653294/safe_bjisiz.svg`,
+    },
+    {
+        title:`X - Ray Chest PA View`,
+        para:`kmdsckdm smas; xas;xmsxjcns `,
+        providedby:`Provided By 4 Labs`,
+        mrf:`840`,
+        off:`40% Off`,
+        img:`https://res.cloudinary.com/du8msdgbj/image/upload/v1590653294/safe_bjisiz.svg`,
+    },
+    
 ]
